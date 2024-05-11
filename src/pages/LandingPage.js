65cccd68e7fbaaf637566e36f2fbc917fa5d801d
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-11 20:15:12 +0800

    added Landing Page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/LandingPage.css'; 
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <img src="/images/PenScan_Logo.png" alt="Landing Page" className="landing-page-image" />
+      <h1 className="landing-page-text">PenScan</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
