48a27376bee765972c4076a53834da48310f3754
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-12 03:21:02 +0800

    displays the classes that the teacher created

--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import "../css/TeacherDashboard.css"; 
+import "../css/TeacherDashboard.css";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 const TeacherDashboard = () => {
-
   const navigate = useNavigate();
   const { username } = useParams();
   const [userId, setUserId] = useState(null);
@@ -13,7 +12,9 @@ const TeacherDashboard = () => {
   useEffect(() => {
     const fetchUserId = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/api/users/getuserid?username=${username}`);
+        const response = await axios.get(
+          `http://localhost:8080/api/users/getuserid?username=${username}`
+        );
         setUserId(response.data);
         console.log(response.data);
       } catch (error) {
@@ -27,9 +28,11 @@ const TeacherDashboard = () => {
   useEffect(() => {
     const fetchUserClasses = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/api/classes/getclassbyteacherid?teacherid=${userId}`);
+        const response = await axios.get(
+          `http://localhost:8080/api/classes/getclassesbyteacherid?teacherid=${userId}`
+        );
         setUserClasses(response.data);
-        console.log(response.data);
+        console.log("User Classes:", response.data); // Add this line
       } catch (error) {
         console.error("Error fetching user classes:", error);
       }
@@ -44,26 +47,41 @@ const TeacherDashboard = () => {
     navigate(`/createclass/${username}`);
   };
 
-
   return (
     <>
-    <div className="teacher-dashboard-container">
-      <div className="logo-container">
-        <img src="/images/PenScan_Logo.png" alt="Logo" className="logo" />
-        <p className="dashboard-text">Dashboard</p>
-      </div>
-      <div className="action-container">
-        <div className="user-icon-container">
-          <img src="/images/UserIcon.png" alt="User Icon" className="user-icon" />
+      <div className="teacher-dashboard-container">
+        <div className="logo-container">
+          <img src="/images/PenScan_Logo.png" alt="Logo" className="logo" />
+          <p className="dashboard-text">Dashboard</p>
+        </div>
+        <div className="action-container">
+          <div className="user-icon-container">
+            <img
+              src="/images/UserIcon.png"
+              alt="User Icon"
+              className="user-icon"
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <div className="classes-container">
-    <p className="classes-text">Classes</p>
-    <img src="/images/AddIcon.png" alt="Plus Icon" className="plus-icon" onClick={handleAddClassClick}/>
-  </div>
-  </>
+      <div className="classes-container">
+        <p className="classes-text">Classes</p>
+        <img
+          src="/images/AddIcon.png"
+          alt="Plus Icon"
+          className="plus-icon"
+          onClick={handleAddClassClick}
+        />
+      </div>
+      <div className="class-names-container">
+        {userClasses.map((classData, index) => (
+          <div className="class-name" key={index}>
+            <div className="class-square">{classData.classname}</div>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default TeacherDashboard;
