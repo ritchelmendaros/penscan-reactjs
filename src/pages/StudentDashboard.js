55a9b70879c9dc6eaf43286e0d6ef1a070eb7b12
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-12 00:41:10 +0800

    initial Student Dashboard

--- src/pages/StudentDashboard.js
+++ src/pages/StudentDashboard.js
@@ -1,10 +1,25 @@
-import '../css/LandingPage.css'; 
+import React from "react";
+import "../css/StudentDashboard.css"; 
 
 const StudentDashboard = () => {
+
   return (
     <>
-    <h2>StudentDashboard</h2>
-    </>
+    <div className="student-dashboard-container">
+      <div className="logo-container">
+        <img src="/images/PenScan_Logo.png" alt="Logo" className="logo" />
+        <p className="dashboard-text">Dashboard</p>
+      </div>
+      <div className="action-container">
+        <div className="user-icon-container">
+          <img src="/images/UserIcon.png" alt="User Icon" className="user-icon" />
+        </div>
+      </div>
+    </div>
+    <div className="classes-container">
+    <p className="classes-text">Classes</p>
+  </div>
+  </>
   );
 }
 
