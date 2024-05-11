f59b0a13be47a0b44d8bd4a9bdd0c773594e8286
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-11 20:41:11 +0800

    added Login.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-    <LandingPage />
+    <Login />
     </>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="content">
+        <img src="/images/PenScan_Logo.png" alt="Logo" className="logo" />
+        <h2 className="login-text">LOGIN</h2>
+        <div className="input-container">
+          <input type="text" placeholder="Username" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <p className="register-text">Doesn't have an account? <a href="/register">Register</a></p>
+          <button className="login-button">LOGIN</button>
+        </div>
+      </div>
+        </div>
+    );
+}
+
+export default Login;
