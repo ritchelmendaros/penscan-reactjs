fc2298bd7623014bc7505e58376f4f5920d400ef
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-13 16:46:55 +0800

    initial implementation for AddStudent.js

--- src/pages/AddStudent.js
+++ src/pages/AddStudent.js
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../css/AddStudent.css";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const AddStudent = () => {
-  return <><h2>Add Student</h2></>;
+  const navigate = useNavigate();
+  const { username } = useParams();
+  const [userId, setUserId] = useState(null);
+  const [classname, setClassname] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/users/getuserid?username=${username}`
+        );
+        setUserId(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching user ID:", error);
+      }
+    };
+
+    fetchUserId();
+  }, [username]);
+
+  const handleStudentnameChange = (event) => {
+    setClassname(event.target.value);
+  };
+
+  const handleDashboardOnclick = () => {
+    navigate(`/teacherdashboard/${username}`);
+  }
+
+  const handleAddStudent = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/classes/checkclass?classname=${classname}&teacherid=${userId}`
+      );
+  
+      if (response.data) {
+        setErrorMessage("Class name already exists.");
+      } else {
+        const addClassResponse = await axios.post(
+          "http://localhost:8080/api/classes/add",
+          { classname, teacherid: userId }
+        );
+        console.log("Class added:", addClassResponse.data);
+        navigate(`/teacherclassfiles/${username}`);
+      }
+    } catch (error) {
+      console.error("Error creating class:", error);
+    }
+  };
+  
+  const handleCloseError = () => {
+    setErrorMessage("");
+  };
+
+  return (
+    <>
+      <div className="addstudent-dashboard-container">
+        <div className="logo-container">
+          <img src="/images/PenScan_Logo.png" alt="Logo" className="logo" onClick={handleDashboardOnclick}/>
+          <p className="dashboard-text" onClick={handleDashboardOnclick}>Dashboard</p>
+        </div>
+        <div className="action-container">
+          <div className="user-icon-container">
+            <img
+              src="/images/UserIcon.png"
+              alt="User Icon"
+              className="user-icon"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="addstudent-container">
+        <p className="addstudent-text">Add Student</p>
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          className="class-name-input"
+          placeholder="Enter Student Name"
+          value={classname}
+          onChange={handleStudentnameChange} 
+        />
+        <button className="addstudent-button" onClick={handleAddStudent}>
+          ADD
+        </button>
+        {errorMessage && (
+          <div className="popup">
+            <div className="popup-content">
+              <p className="error-message">{errorMessage}</p>
+              <button className="ok-button" onClick={handleCloseError}>
+                OK
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default AddStudent;
